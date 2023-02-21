d442c9c72794f564dd7655e13dc978f11e4abad2
root <root@kali.school.net>
2023-02-21 15:21:01 -0500

    updated readme file with some instructions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,17 @@
 
 void print_to_98(int n)
 {
-	while (n != 98)
+	if (n < 98)
 	{
-		if (n < 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
-		else if (n > 98)
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
