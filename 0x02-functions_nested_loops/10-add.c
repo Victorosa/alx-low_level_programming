6f955b1f374c2f8ea22f69b4102fbbda916aef82
root <root@kali.school.net>
2023-02-21 14:35:13 -0500

    updated readme file with some instructions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - get the absolute value of an integer
+ * @a: accept an integer
+ * @b: accept an integer
+ *
+ * Return: 1 or 0
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
