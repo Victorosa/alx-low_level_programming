93b8dfe121f33feec0413fc37e2c70a71faa8cdc
Prom1234pro <promiseimadonmwinyi@gmail.com>
2023-02-27 15:31:16 -0500

    alx-project

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 /**
- * main - Convert a string to an integer.
+
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
+
 {
-    printf("sdhasidfa");
+    int pass[100];
+    int i, sum, n;
+
+    sum = 0;        
+
+    srand(time(NULL));
+
+    for (i = 0; i < 100; i++)
+    {
+        pass[i] = rand() % 78;
+        sum += (pass[i] + '0');
+        putchar(pass[i] + '0');
+        if ((2772 - sum) - '0' < 78)
+        {
+            n = 2772 - sum - '0';
+            sum += n;
+            putchar(n + '0');
+            break;
+        }
+    }
+    return (0);
 }
