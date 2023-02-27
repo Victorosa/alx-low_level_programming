2eb5902a653ddaea24dd9232031d1ff6d02a171d
Prom1234pro <promiseimadonmwinyi@gmail.com>
2023-02-27 14:02:49 -0500

    alx-project

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,17 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
+	int c = 0;
 
-	while (s[i] != '\0')
+	while (s[c] != '\0')
 	{
-		i++;
+		c++;
 	}
-	i--;
-	while (i >= 0)
+
+	for (c -= 1; c >= 0; c--)
 	{
-		_putchar(s[i]);
-		i--;
+		_putchar(s[c]);
 	}
+
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,17 @@
 void puts2(char *str)
 {
 	int i;
+	int j = 0;
 
-	i = 0;
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		j++;
 	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
