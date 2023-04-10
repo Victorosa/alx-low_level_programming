1302055af29447574a2e013c0c4073403858daba
victorosa <victorimadon@gmail.com>
2023-04-10 16:37:35 -0700

    dd

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,33 @@
-#include "main.h"
+    #include <stddef.h>
+    #include "main.h"
 
-/**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: Pointer to a string of 0 and 1 chars.
- *
- * Return: The converted number, or 0 if there is one or more chars in the string
- *         b that is not 0 or 1, or if b is NULL.
- */
-unsigned int binary_to_uint(const char *b)
-{
-    unsigned int num = 0;
-
-    if (b == NULL)
-        return 0;
-
-    while (*b != '\0')
+    /**
+     * binary_to_uint - Converts a binary number to an unsigned int.
+     * @b: Pointer to a string of 0 and 1 chars.
+     *
+     * Return: The converted number, or 0 if there is one or more chars in the string
+     *         b that is not 0 or 1, or if b is NULL.
+     */
+    unsigned int binary_to_uint(const char *b)
+      
     {
-        if (*b == '0' || *b == '1')
-        {
-            num = num * 2 + (*b - '0');
-            b++;
-        }
-        else
-        {
-            return 0;
-        }
-    }
+        unsigned int num;
+        int a, c;
 
-    return num;
+        if (b == NULL)
+            return (0);
+         a = 0;
+        while (b[a + 1] != '\0')
+		        i++;
+        c = 1;
+        num = 0;
+        while (a >= 0)
+       {
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		num += (b[i] - '0') * c;
+		c *= 2;
+		a--;
+	}
+	return (num);
 }
